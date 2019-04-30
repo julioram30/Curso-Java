c1badd993c5cdcfdd5c08abc64cfc784a37cdd12
julio.martinez <julio.martinez@EQA12.eivsoftware.net>
2019-04-30 14:48:28 -0300

    creacion estrde datos prestamo

--- EjercicioPrestamo/src/main/java/com/eiv/entidad/Prestamo.java
+++ EjercicioPrestamo/src/main/java/com/eiv/entidad/Prestamo.java
@@ -1,8 +1,43 @@
 package com.eiv.entidad;
 
+import java.math.BigDecimal;
+
 public class Prestamo {
 
     private int Cuota;
-    // private VC  Interes Capital Saldo
+    private BigDecimal VC;
+    private BigDecimal Interes;
+    private BigDecimal Capital;
+    private BigDecimal Saldo;
+    public int getCuota() {
+        return Cuota;
+    }
+    public void setCuota(int cuota) {
+        Cuota = cuota;
+    }
+    public BigDecimal getVC() {
+        return VC;
+    }
+    public void setVC(BigDecimal vC) {
+        VC = vC;
+    }
+    public BigDecimal getInteres() {
+        return Interes;
+    }
+    public void setInteres(BigDecimal interes) {
+        Interes = interes;
+    }
+    public BigDecimal getCapital() {
+        return Capital;
+    }
+    public void setCapital(BigDecimal capital) {
+        Capital = capital;
+    }
+    public BigDecimal getSaldo() {
+        return Saldo;
+    }
+    public void setSaldo(BigDecimal saldo) {
+        Saldo = saldo;
+    }
 
 }
